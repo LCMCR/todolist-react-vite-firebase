8a195e54bf6fd530a4e39f2f9aee17d6c3173fac
Laura Collazo <laucollazo@hotmail.com>
2024-01-17 12:07:14 -0300

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # React + Vite + Firebase 10
 
-This template provides a minimal setup to get React working in Vite and Firebase 10 and Firestore for authentication and real-time database usage.
+## To do list
+
+This to do list allows users to login with their Google account and add, edit, and delete tasks.
+Firebase is used for authentication and database. Firestore, the database, is NoSQL.
+The app is built with React and Vite.
+
+Some components are from Mantine.
+Tabler Icons are used for the icons. And react-loader-spinner is used for the loading spinner.
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth"
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDy6uh3a8Qxpvp0bQmRpjZjpaD9ixXVpvw",
-  authDomain: "modern-todolist-d080c.firebaseapp.com",
-  projectId: "modern-todolist-d080c",
-  storageBucket: "modern-todolist-d080c.appspot.com",
-  messagingSenderId: "578138999660",
-  appId: "1:578138999660:web:f8e96926369882c62f0149"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
